6332e9f02c02d5b7eabcb4bf107d17f895dcf4a3
omartins-zs <gabrielmatheus209@gmail.com>
2023-09-21 16:12:22 -0300

    Create Function Get API Fetch BD

--- application/controllers/api/ApiEmployeeController.php
+++ application/controllers/api/ApiEmployeeController.php
@@ -7,8 +7,19 @@ use chriskacerguis\RestServer\RestController;
 
 class ApiEmployeeController extends RestController
 {
+	public function __construct()
+	{
+		parent::__construct();
+		$this->load->model('EmployeeModel');
+	}
+
 	public function index_get()
 	{
-		echo "I am Employee inde function API";
+		// echo "I am Employee inde function API";
+		$employee = new EmployeeModel;
+		$result_emp = $employee->get_employee();
+		$this->response($result_emp, 200);
 	}
 }
+
+?>
