2adfb3ea7e7d19a85842e5b1a821c3ac062ec56f
omartins-zs <gabrielmatheus209@gmail.com>
2023-09-21 09:10:55 -0300

    Criando ApiDemoController e Teste Function index

--- application/controllers/api/ApiDemoController.php
+++ application/controllers/api/ApiDemoController.php
@@ -1,7 +1,15 @@
 <?php
+defined('BASEPATH') or exit('No direct script access allowed');
+
+require APPPATH . 'libraries/RestController.php';
 
 use chriskacerguis\RestServer\RestController;
 
-class ApiDemoController extends RestController{
-	
+class ApiDemoController extends RestController
+{
+	public function index()
+
+	{
+		echo "I am RESTful API";
+	}
 }
