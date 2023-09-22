4de1572a93058bd2f6b68523d91aca9e19a57e51
omartins-zs <gabrielmatheus209@gmail.com>
2023-09-22 09:11:34 -0300

    Fazendo Teste Function store post Employee

--- application/controllers/api/ApiEmployeeController.php
+++ application/controllers/api/ApiEmployeeController.php
@@ -20,6 +20,18 @@ class ApiEmployeeController extends RestController
 		$result_emp = $employee->get_employee();
 		$this->response($result_emp, 200);
 	}
-}
 
-?>
+	public function store_employee_post()
+	{
+		// echo "I am Employee inde function API";
+		$data = [
+			'nome' => $this->input->post('nome'),
+			'sobrenome' => $this->input->post('sobrenome'),
+			'celular' => $this->input->post('celular'),
+			'email' => $this->input->post('email'),
+		];
+		// new EmployeeModel;
+		// $result_emp = $employee->get_employee();
+		$this->response($data, 200);
+	}
+}
