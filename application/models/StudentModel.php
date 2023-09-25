68445e8a7acad8255f3332eca2190ed7be006629
omartins-zs <gabrielmatheus209@gmail.com>
2023-09-25 09:26:47 -0300

    Criando Metodo Edit e Update de Students  no BD

--- application/models/StudentModel.php
+++ application/models/StudentModel.php
@@ -14,4 +14,17 @@ class StudentModel extends CI_Model
 	{
 		return $this->db->insert('students', $data);
 	}
+
+	public function edit_student($id)
+	{
+		$this->db->where('id', $id);
+		$query = $this->db->get('students');
+		return $query->row();
+	}
+
+	public function update_student($data, $id)
+	{
+		$this->db->where('id', $id);
+		return $this->db->update('students', $data);
+	}
 }
