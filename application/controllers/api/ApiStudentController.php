8d8036b513eb51c7af4b200146da138d86aaad4b
omartins-zs <gabrielmatheus209@gmail.com>
2023-09-25 09:21:26 -0300

    Criando Function Insert de Students

--- application/controllers/api/ApiStudentController.php
+++ application/controllers/api/ApiStudentController.php
@@ -13,4 +13,26 @@ class ApiStudentController extends RestController
 		parent::__construct();
 		$this->load->model('StudentModel');
 	}
+
+	public function storeStudent_post()
+	{
+		$students = new StudentModel;
+		$data = [
+			'name' =>  $this->input->post('name'),
+			'class' => $this->input->post('class'),
+			'email' => $this->input->post('email')
+		];
+		$result = $students->insert_student($data);
+		if ($result > 0) {
+			$this->response([
+				'status' => true,
+				'message' => 'NEW STUDENT CREATED'
+			], RestController::HTTP_OK);
+		} else {
+			$this->response([
+				'status' => false,
+				'message' => 'FAILED TO CREATE NEW STUDENT'
+			], RestController::HTTP_BAD_REQUEST);
+		}
+	}
 }
